ea2928066169ae1d7d50e9cd33d9940fd05fb04a
Kabillan T A <kabillan1905@gmail.com>
2024-09-17 05:37:17 +0530

    update

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ llm = ChatGroq(
     model="llama-3.1-70b-versatile",
     temperature=1,
     max_tokens=4096,  # Adjusted to the correct max tokens limit
-    groq_api_key="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"
+    groq_api_key="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"
 )
 
 if st.button("Submit"):
     if prompt:
-        response = llm.invoke("Generate a journal summary based on the following points: " + prompt)
+        response = llm.invoke(prompt)
         
         st.write("Response:\n")
         st.write(response.content)
